40da7ebe2a9c23071b33da3290a7129773325bfb
Hasina <67200805+hs-x11@users.noreply.github.com>
2023-08-31 14:20:26 -0400

    Added a new function called singleMatch

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-
-function rpsMatch(playerSelection, computerSelection) {
-
-}
-
 let choiceRPS = ['Rock', 'Paper', 'Scissor'];
 function getComputerChoice() {
-    console.log(choiceRPS[(Math.floor(Math.random() * choiceRPS.length))]);
+    return(choiceRPS[(Math.floor(Math.random() * choiceRPS.length))]);
 }
-getComputerChoice()
+
+let playerSelection = prompt('Choose Rock, Paper, or Scissor');
+let computerSelection = getComputerChoice();
+function singleMatch (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper'){
+        return 'You Lose! Paper beats Rock'
+    }
+}
+console.log(singleMatch (playerSelection, computerSelection));
