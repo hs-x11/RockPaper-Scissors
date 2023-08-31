bb68a6fdee8bd3d3e85912341f63419c20951b17
Hasina <67200805+hs-x11@users.noreply.github.com>
2023-08-31 15:16:44 -0400

    Removed some redundancy in the function code

--- script.js
+++ script.js
@@ -3,35 +3,27 @@ function getComputerChoice() {
     return(choiceRPS[(Math.floor(Math.random() * choiceRPS.length))]);
 }
 
-let playerSelection = prompt('Choose Rock, Paper, or Scissor');
+let playerResponse = prompt('Choose Rock, Paper, or Scissor');
 const computerSelection = getComputerChoice();
+const playerSelection = playerResponse.toLowerCase();
+
 function singleRoundGame (playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
+    if (playerSelection === computerSelection ) {
         return `No one wins it's a draw`;
 
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        return 'You Lose! Paper beats Rock';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissor') || 
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissor' && computerSelection == 'paper') 
+    ) {
+        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
 
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissor') {
-        return 'You Win! Paper beats Scissor';
-
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        return 'You Win! Paper beats Rock';   
-
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
-        return `No one wins it's a draw`;
-
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissor') {
-        return 'You Lose! Scissor beats Paper';
-
-    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'rock') {
-        return 'You Lose! Rock beats Scissor';   
-
-    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'paper') {
-        return 'You win! Scissor beats Paper';
-
-    } else if (playerSelection.toLowerCase() === 'scissor' && computerSelection === 'scissor') {
-        return `No one wins it's a draw`;
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissor') ||
+        (playerSelection === 'scissor' && computerSelection === 'rock')
+    ) {
+        return `You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
 
     } else {
         return 'You did not enter a valid response'; 
